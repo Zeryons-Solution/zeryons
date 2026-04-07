da588bfb3bd4b82526452a8a0140de02843d5264
Sambhav Wakode <mitul.wakode@gmail.com>
2026-04-07 23:22:19 +0530

    Fix ALLOWED_HOSTS for Render deployment

--- Zeryons/settings.py
+++ Zeryons/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['https://zeryons.onrender.com']
+ALLOWED_HOSTS = ['zeryons.onrender.com']
 
 
 # Application definition
