7a825fe2da0b8079db8530f5c3b891e984fc4539
Sambhav Wakode <mitul.wakode@gmail.com>
2026-04-09 01:19:10 +0530

    add whitenoise

--- Zeryons/settings.py
+++ Zeryons/settings.py
@@ -141,10 +141,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # For production static files
 # settings.py
-# STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 WHITENOISE_MANIFEST_STRICT = False
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
