75cb12a91c0c376c66712cf87e81ccf72e225ee9
Sambhav Wakode <mitul.wakode@gmail.com>
2026-04-09 14:15:12 +0530

    Updated

--- Zeryons/settings.py
+++ Zeryons/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['zeryons.onrender.com']
-
+ALLOWED_HOSTS = ['zeryons.onrender.com', '.onrender.com']
 
 # Application definition
 
